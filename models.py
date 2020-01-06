332ec082553851a393a4d434ea64602c099d6b84
shayan <shekshaa.75@gmail.com>
2020-01-06 19:30:11 +0330

    EFGCN_MLGCN model

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 from gcn.layers import GraphConvolution
-from gcn.inits import glorot, zeros
+from gcn.inits import glorot
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 
-class WeightedAutoencoder(object):
+class EFGCN_MLGCN(object):
     def __init__(self, name, placeholders, num_nodes, super_mask, use_weight,
                  featureless=True, activation=tf.nn.tanh, bias=True):
         self.name = name
@@ -18,23 +18,20 @@ class WeightedAutoencoder(object):
         self.num_features_nonzero = placeholders['num_features_nonzero']
 
         # adjacency matrix
-        self.support = placeholders['support']
+        self.EFGCN_support = placeholders['support']
+        self.MLGCN_support = [placeholders['support0'], placeholders['support1'], placeholders['support2']]
         self.edge_labels = placeholders['edge_labels']
         self.edge_mask = placeholders['edge_mask']
 
         # node type variables
         self.node_types = placeholders['node_types']
-        self.n_types = self.node_types.get_shape().as_list()[1]
-
-        # node label variables
-        self.node_labels = placeholders['node_labels']
-        self.n_labels = self.node_labels.get_shape().as_list()[1]
+        self.n_types = len(self.MLGCN_support)
 
         # network architectural settings
         self.use_weight = use_weight
         self.activation = activation
-        self.base_gc_dropout = placeholders['base_gc_dropout']
-        self.node_gc_dropout = placeholders['node_gc_dropout']
+        self.EFGCN_dropout = placeholders['EFGCN_dropout']
+        self.MLGCN_dropout = placeholders['MLGCN_dropout']
         self.super_mask = super_mask
         self.featureless = featureless
         self.bias = bias
@@ -44,14 +41,11 @@ class WeightedAutoencoder(object):
         self.h1 = None
         self.h2 = None
         self.w = {}
-        self.layers = []
-        self.node_label_logits = None
-        self.edge_module_input_type = None
+        self.EFGCN_layers = []
+        self.MLGCN_layers = {}
+        self.EFGCN_output_separated = []
+        self.MLGCN_output = []
         self.edge_logits = {}
-        self.node_label_module_input = None
-        self.edge_module_input = None
-        self.label_acc = 0
-        self.label_loss = 0
         self.recall = 0
         self.precision = 0
         self.f1 = 0
@@ -61,72 +55,68 @@ class WeightedAutoencoder(object):
         # build inference mode and loss and accuracy
         self.build()
         self.loss()
-        self.acc()
+        # self.acc()
         self.precision_recall_f1()
         self.summary1, self.summary2 = self.create_summary()
 
         self.opt = self.optimizer.minimize(self.total_loss)
 
     def build(self):
-        layer_placeholders = {
-            'support': self.support,
-            'dropout': self.base_gc_dropout,
+        EFGCN_placeholders = {
+            'support': self.EFGCN_support,
+            'dropout': self.EFGCN_dropout,
             'num_features_nonzero': self.num_features_nonzero
         }
 
-        self.layers.append(GraphConvolution(input_dim=self.n_features,
-                                            output_dim=FLAGS.hidden1,
-                                            placeholders=layer_placeholders,
-                                            dropout=False,
-                                            act=self.activation,
-                                            bias=self.bias,
-                                            logging=True,
-                                            featureless=self.featureless
-                                            ))
+        self.EFGCN_layers.append(GraphConvolution(input_dim=self.n_features,
+                                                  output_dim=FLAGS.hidden1,
+                                                  placeholders=EFGCN_placeholders,
+                                                  dropout=False,
+                                                  act=self.activation,
+                                                  bias=self.bias,
+                                                  logging=True,
+                                                  featureless=self.featureless
+                                                  ))
 
-        self.layers.append(GraphConvolution(input_dim=FLAGS.hidden1,
-                                            output_dim=FLAGS.hidden2,
-                                            placeholders=layer_placeholders,
-                                            dropout=True,
-                                            act=self.activation,
-                                            bias=self.bias,
-                                            logging=True))
+        self.EFGCN_layers.append(GraphConvolution(input_dim=FLAGS.hidden1,
+                                                  output_dim=FLAGS.hidden2,
+                                                  placeholders=EFGCN_placeholders,
+                                                  dropout=True,
+                                                  act=self.activation,
+                                                  bias=self.bias,
+                                                  logging=True))
 
-        self.h1 = self.layers[0](self.features)
-        self.h2 = self.layers[1](self.h1)
+        self.h1 = self.EFGCN_layers[0](self.features)
+        self.h2 = self.EFGCN_layers[1](self.h1)
 
-        type_fd_placeholders = {
-            'support': self.support,
-            'dropout': self.node_gc_dropout,
-            'num_features_nonzero': self.num_features_nonzero
-        }
-        self.node_label_module_input = self.h2
+        EFGCN_output = self.h2
+        self.EFGCN_output_separated = [tf.boolean_mask(tensor=EFGCN_output, mask=self.node_types[:, i])
+                                       for i in range(self.n_types)]
 
-        self.layers.append(GraphConvolution(input_dim=FLAGS.hidden2,
-                                            output_dim=self.n_types,
-                                            placeholders=type_fd_placeholders,
-                                            dropout=True,
-                                            act=lambda x: x,
-                                            bias=self.bias,
-                                            logging=True))
+        for i in range(self.n_types):
+            MLGCN_placeholders = {
+                'support': self.MLGCN_support[i],
+                'dropout': self.MLGCN_dropout,
+                'num_features_nonzero': self.num_features_nonzero
+            }
+            self.MLGCN_layers['{}'.format(i)] = GraphConvolution(input_dim=FLAGS.hidden2,
+                                                                 output_dim=FLAGS.hidden3,
+                                                                 placeholders=MLGCN_placeholders,
+                                                                 dropout=False,
+                                                                 act=lambda x: x,
+                                                                 bias=self.bias,
+                                                                 logging=True)
 
-        self.node_label_logits = self.layers[2](self.node_label_module_input)
+            self.MLGCN_output.append(self.MLGCN_layers['{}'.format(i)](self.EFGCN_output_separated[i]))
 
-        self.edge_module_input = self.h2
         with tf.variable_scope(self.name):
-            n_features = self.edge_module_input.get_shape().as_list()[1]
+            n_features = FLAGS.hidden3
             for i in range(self.n_types):
                 for j in range(i, self.n_types):
                     if self.super_mask[i][j]:
-                        # var = glorot(shape=(n_features, n_features), name='w_{}_{}'.format(i, j))
-                        var = glorot(shape=(n_features, 1), name='w_{}_{}'.format(i, j))
-                        self.w['{}_{}'.format(i, j)] = tf.matrix_diag(var)
+                        var = glorot(shape=(n_features, n_features), name='w_{}_{}'.format(i, j))
                         tf.summary.histogram(name='w_{}_{}'.format(i, j), values=var)
-                        # self.w['{}_{}'.format(i, j)] = (var + tf.transpose(var)) / 2.
-                        # self.w['{}_{}'.format(i, j)] = var
-
-        self.edge_module_input_type = [tf.boolean_mask(tensor=self.edge_module_input, mask=self.node_types[:, i])
-                                       for i in range(self.n_types)]
+                        self.w['{}_{}'.format(i, j)] = (var + tf.transpose(var)) / 2.
 
         self.edge_logits = dict()
         for i in range(self.n_types):
@@ -134,16 +124,13 @@ class WeightedAutoencoder(object):
                 if self.super_mask[i][j]:
                     if self.use_weight:
                         weight = self.w['{}_{}'.format(i, j)]
-                        self.edge_logits['{}_{}'.format(i, j)] = tf.matmul(
-                            tf.matmul(self.edge_module_input_type[i], weight)
-                            , tf.transpose(self.edge_module_input_type[j]))
+                        self.edge_logits['{}_{}'.format(i, j)] = tf.matmul(tf.matmul(self.MLGCN_output[i], weight),
+                                                                           tf.transpose(self.MLGCN_output[j]))
                     else:
-                        self.edge_logits['{}_{}'.format(i, j)] = tf.matmul(self.edge_module_input_type[i],
-                                                                           tf.transpose(self.edge_module_input_type[j]))
+                        self.edge_logits['{}_{}'.format(i, j)] = tf.matmul(self.MLGCN_output[i],
+                                                                           tf.transpose(self.MLGCN_output[j]))
 
     def loss(self):
-        self.label_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.node_label_logits,
-                                                                                 labels=self.node_types))
         self.total_edge_loss = 0
         for i in range(self.n_types):
             for j in range(i, self.n_types):
@@ -159,30 +146,27 @@ class WeightedAutoencoder(object):
                     self.total_edge_loss += tf.reduce_mean(self.edge_mask['adj_{}_{}'.format(i, j)] * non_mask_loss)
 
         l2_reg = 0
-        for var in self.layers[0].vars.values():
+        for var in self.EFGCN_layers[0].vars.values():
             l2_reg += tf.nn.l2_loss(var)
 
-        # weight_loss = 0
-        # for _, weight in self.w.items():
-        #     weight_loss += tf.norm(weight)
+        # for layer in self.MLGCN_layers:
+        #     l2_reg += tf.nn.l2_loss(layer.vars.values())
 
-        self.total_loss = FLAGS.lmbda * self.label_loss + self.total_edge_loss + FLAGS.weight_decay * l2_reg
+        self.total_loss = self.total_edge_loss + FLAGS.weight_decay * l2_reg
 
     def create_summary(self):
         summary1_list = [tf.summary.scalar(name='total_edge_loss', tensor=self.total_edge_loss),
                          tf.summary.scalar(name='total_loss', tensor=self.total_loss)]
-        if FLAGS.lmbda > 0:
-            summary1_list += [tf.summary.scalar(name='node_label_loss', tensor=self.label_loss),
-                              tf.summary.scalar(name='node_label_acc', tensor=self.label_acc)]
+
         summary2_list = [tf.summary.scalar(name='precision', tensor=self.precision),
                          tf.summary.scalar(name='recall', tensor=self.recall),
                          tf.summary.scalar(name='F1', tensor=self.f1)]
         return tf.summary.merge(summary1_list), tf.summary.merge(summary2_list)
 
-    def acc(self):
-        label_correct_predictions = tf.equal(tf.argmax(self.node_label_logits, 1),
-                                             tf.argmax(self.node_types, 1))
-        self.label_acc = tf.reduce_mean(tf.cast(label_correct_predictions, dtype=tf.float32))
+    # def acc(self):
+    #     label_correct_predictions = tf.equal(tf.argmax(self.node_label_logits, 1),
+    #                                          tf.argmax(self.node_types, 1))
+    #     self.label_acc = tf.reduce_mean(tf.cast(label_correct_predictions, dtype=tf.float32))
 
     def precision_recall_f1(self):
         true_positive = true_negative = false_positive = false_negative = 0
